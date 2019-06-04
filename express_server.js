a14921d73d129f3b26a3429e3036cf393608e18d
Aidanchase <aidanmro@gmail.com>
2019-06-04 18:34:01 +0000

    created table with links and shortened links

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 var urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -12,12 +12,16 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls", (req, res) => {
-  let templateVars = { hello: 'world',
-  urls: urlDatabase 
+  let templateVars = {
+    urls: urlDatabase 
 };
-console.log(urlDatabase);
-  res.render("urls_index", templateVars)
+res.render("urls_index", templateVars)
 });
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],  
+     };
+    res.render("urls_show", templateVars);
+  });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
