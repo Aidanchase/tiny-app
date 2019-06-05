e5a96a19eccab799a805eba710106037d464210b
Aidanchase <aidanmro@gmail.com>
2019-06-05 03:08:27 +0000

    added a redirect from short url to long url! Everything is in order :)

--- express_server.js
+++ express_server.js
@@ -10,7 +10,7 @@ function generateRandomString(length, chars) {
   for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
   return result;
 }
-let urlDatabase = {
+const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -19,13 +19,14 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.post("/urls", (req, res) => {
-  urlDatabase["shortURL"] = generateRandomString("6", "123456789abcdefghijklmnopqrstuvwxyz"); 
-  urlDatabase["longURL"] = req.body;
-  console.log(urlDatabase); // Respond with 'Ok' (we will replace this)
+  let randomString = generateRandomString("6", "123456789abcdefghijklmnopqrstuvwxyz");
+  urlDatabase[randomString] = req.body.longURL; 
+  res.redirect(`/urls/${randomString}`);
 });
+
 app.get("/urls", (req, res) => {
   let templateVars = {
-    urls: urlDatabase
+    urls: urlDatabase,
   };
 
   res.render("urls_index", templateVars);
@@ -43,8 +44,10 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
-
-
+app.get("/u/:shortURL", (req, res) => {
+    const longURL = urlDatabase[req.params.shortURL];
+     res.redirect(longURL);
+   });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
