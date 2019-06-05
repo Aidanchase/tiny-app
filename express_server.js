99833718b60d28f684eba341e5b25b817c12338b
Aidanchase <aidanmro@gmail.com>
2019-06-05 19:33:18 +0000

    changed background color for urls_show, delete button now working

--- express_server.js
+++ express_server.js
@@ -46,7 +46,10 @@ app.get("/urls/new", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
+});
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
@@ -54,10 +57,9 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
-app.post("/urls/:shortURL/delete", (req, res) =>{
-  res.delete(longURL);
-})
+
