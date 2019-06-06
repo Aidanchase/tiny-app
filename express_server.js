c8d2c5860fc941f567bc83599f49f32354b82131
Aidanchase <aidanmro@gmail.com>
2019-06-06 17:45:21 +0000

    added  registration handler, now sends  400  error  if user  is already registered or  doesn 't enter info

--- express_server.js
+++ express_server.js
@@ -14,8 +14,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const users =  {
-}
+const users = {}
 
 function generateRandomString(length, chars) {
   let result = "";
@@ -24,6 +23,13 @@ function generateRandomString(length, chars) {
   return result;
 }
 
+function emailLookup(email){
+  for (let i in users){
+    if (users[i].email === email){
+      return i;
+    }
+ }
+}
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
@@ -54,7 +60,7 @@ app.get("/urls.json", (req, res) => {
 });
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    email: req.cookies["user_id"]
+    users
   }
   res.render("urls_new", templateVars);
 });
@@ -74,16 +80,19 @@ app.post("/logout", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("registration_page");
 })
-app.post("/register", (req, res) =>{ 
-const randomID = generateRandomString(8, "123456789abcdefghijklmnopqrstuvwxyz")
+app.post("/register", (req, res) => {
+  const randomID = generateRandomString(8, "123456789abcdefghijklmnopqrstuvwxyz");
+  if(req.body.email === "" || req.body.password === "" || emailLookup(req.body.email)) {
+    res.status(400).send("Please enter your info or check that you're not already registered");
+  } else {
 users[randomID] = {
-  id: randomID,
-  email: req.body.email,
-  password: req.body.password,
-}
-res.cookie("user_id", randomID);
-
-res.redirect("/urls");
+    id: randomID,
+    email: req.body.email,
+    password: req.body.password,
+  }
+  res.cookie("user_id", randomID);
+  res.redirect("/urls");
+  }
 })
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
@@ -105,4 +114,3 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls");
 });
-
