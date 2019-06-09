11e79b5912800690f979ceddbc8efbef9b2e9707
Aidanchase <aidanmro@gmail.com>
2019-06-09 13:37:16 +0000

    Improved code readability and ejs files

--- express_server.js
+++ express_server.js
@@ -1,20 +1,26 @@
 let express = require("express");
-let app = express();
-let PORT = 8080; // default port 8080
-app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
+
 const cookieSession = require("cookie-session");
+
 const bcrypt = require("bcrypt");
+
+let app = express();
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(cookieSession({
   name: "session",
   keys: ["key1", "key2"],
-
+  
   maxAge: 24 * 60 * 60 * 1000
 }));
+
+app.set("view engine", "ejs");
+
+let PORT = 8080; 
 
 const urlDatabase = {
   "b2xVn2": {
@@ -29,7 +35,7 @@ const urlDatabase = {
     longURL: "http://www.reddit.com",
     userID: "hhjsjs"
   }
-}
+};
 // Adds user info on login
 const users = {};
 // generate random user id
@@ -38,7 +44,7 @@ function generateRandomString(length, chars) {
   for (var i = length; i > 0; --i)
     result += chars[Math.floor(Math.random() * chars.length)];
   return result;
-}
+};
 
 function emailLookup(email) {
   for (let i in users) {
@@ -46,7 +52,7 @@ function emailLookup(email) {
       return i;
     }
   }
-}
+};
 // filter urls to be displayed for each user
 const urlsForUser = function (id) {
   const userURLS = {};
@@ -56,14 +62,8 @@ const urlsForUser = function (id) {
     }
   }
   return userURLS;
-}
+};
 
-
-
-
-app.listen(PORT, () => {
-  console.log(`TinyApp listening on port ${PORT}!`);
-});
 app.get("/", (req, res) => {
   if (users[req.session.user_id]) {
     res.redirect("/urls");
@@ -71,6 +71,7 @@ app.get("/", (req, res) => {
     res.redirect("/login");
   }
 });
+
 app.get("/urls", (req, res) => { //get and render unique user index page
   const user_id = req.session.user_id;
   if (users[req.session.user_id]) {
@@ -83,22 +84,24 @@ app.get("/urls", (req, res) => { //get and render unique user index page
     res.redirect("/login");
   };
 });
+
 app.post("/urls", (req, res) => { // adds user generated urls to url DB
   let randomString = generateRandomString(
     "6",
     "123456789abcdefghijklmnopqrstuvwxyz"
   );
+  
   const currentUser = req.session.user_id;
   urlDatabase[randomString] = {};
   urlDatabase[randomString]["longURL"] = req.body.longURL;
   urlDatabase[randomString]["userID"] = currentUser;
   res.redirect(`/urls/${randomString}`);
-
 });
 
 app.get("/urls.json", (req, res) => { //json object of urls
   res.json(urlDatabase);
 });
+
 app.get("/urls/new", (req, res) => {
   if (!req.session.user_id) {
     res.redirect("/login")
@@ -109,10 +112,12 @@ app.get("/urls/new", (req, res) => {
   }
   res.render("urls_new", templateVars);
 });
+
 app.get("/login", (req, res) => {
   const user_id = req.session.user_id;
   res.render("login_page")
-})
+});
+
 app.post("/login", (req, res) => { // user email and password in database checked against input
   const password = req.body.password;
   const userId = emailLookup(req.body.email)
@@ -123,13 +128,16 @@ app.post("/login", (req, res) => { // user email and password in database checke
     res.status(400).send("You don't have an account yet.")
   }
 });
+
 app.post("/logout", (req, res) => { //clear user info on logout
   req.session.user_id = null;
   res.redirect("/");
-})
+});
+
 app.get("/register", (req, res) => {
   res.render("registration_page");
-})
+});
+
 app.post("/register", (req, res) => { // user info logged on register, password hashed and random  id set.
   const password = req.body.password;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -146,7 +154,8 @@ app.post("/register", (req, res) => { // user info logged on register, password 
     req.session.user_id = randomID;
     res.redirect("/urls");
   }
-})
+});
+
 app.post("/urls/:shortURL/delete", (req, res) => { //delete user short urls
   if (!req.session.user_id) {
     res.redirect("/login");
@@ -155,6 +164,7 @@ app.post("/urls/:shortURL/delete", (req, res) => { //delete user short urls
     res.redirect("/urls");
   }
 });
+
 app.get("/urls/:shortURL", (req, res) => { //show individual short urls
   const user_id = req.session.user_id;
   let templateVars = {
@@ -166,11 +176,12 @@ app.get("/urls/:shortURL", (req, res) => { //show individual short urls
   // console.log(templateVars);
   res.render("urls_show", templateVars);
 });
+
 app.get("/u/:shortURL", (req, res) => {
-  ;
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(`${longURL}`)
 });
+
 app.post("/urls/:id", (req, res) => { //redirects  to login if there is no user id present
   const user_id = req.session.user_id;
   if (!user_id) {
@@ -178,4 +189,8 @@ app.post("/urls/:id", (req, res) => { //redirects  to login if there is no user 
   }
   urlDatabase[req.params.id]["longURL"] = req.body.longURL;
   res.redirect("/urls");
+});
+
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
